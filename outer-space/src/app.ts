52c80703a8a2b5beacba57ca2c84711977ea26b6
Ben Walker <bwalke05@gmail.com>
2019-05-09 19:16:02 -0400

    Enable cors

--- outer-space/src/app.ts
+++ outer-space/src/app.ts
@@ -1,7 +1,12 @@
+/* tslint:disable:ordered-imports */
+import 'dotenv/config';
+import cors from './config/cors';
 import * as express from 'express';
 import * as helmet from 'helmet';
 
 const app = express();
 app.use(helmet());
+app.options('*', cors);
+app.use(cors);
 
 export default app;

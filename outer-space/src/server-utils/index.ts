a5fed573f361b4b9b4523b43aa62ae9f64ce73d6
Ben Walker <bwalke05@gmail.com>
2019-05-08 23:00:53 -0400

    Setup foundation with webpack + typescript

---
+++ outer-space/src/server-utils/index.ts
@@ -0,0 +1,9 @@
+import normalizePort from './normalize-port';
+import onError from './on-error';
+import onListening from './on-listening';
+
+export {
+  normalizePort,
+  onError,
+  onListening,
+};

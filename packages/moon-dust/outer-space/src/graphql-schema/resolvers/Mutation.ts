a7b31f064454a0473640317d80f67591baa75e78
Ben Walker <bwalke05@gmail.com>
2019-05-21 15:05:29 -0400

    Customize Mutation, expose createUser and deleteUser

---
+++ packages/moon-dust/outer-space/src/graphql-schema/resolvers/Mutation.ts
@@ -0,0 +1,13 @@
+import { prismaObjectType } from 'nexus-prisma';
+
+const Mutation = prismaObjectType({
+  name: 'Mutation',
+  definition (t) {
+    t.prismaFields([
+      'createUser',
+      'deleteUser',
+    ]);
+  },
+});
+
+export default Mutation;

6c7e78de65b925bdfb80c8c16fa54b4efac8f073
Ben Walker <bwalke05@gmail.com>
2019-06-22 20:35:54 -0400

    Replace css styling with styled components

--- capsules/moon-dust/src/components/App/App.tsx
+++ capsules/moon-dust/src/components/App/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 import logo from '../../assets/logo.svg';
-import './App.css';
+
+const StyledApp = styled.div`
+  text-align: center;
+`;
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 120s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <StyledApp>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </StyledApp>
   );
 };
 

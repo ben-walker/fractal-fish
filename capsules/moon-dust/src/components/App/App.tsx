7c1ba1ef9a0b5632b126bca3f82c5b07ae762ebc
Ben Walker <bwalke05@gmail.com>
2019-06-23 15:37:41 -0400

    Trim down App component

--- capsules/moon-dust/src/components/App/App.tsx
+++ capsules/moon-dust/src/components/App/App.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import tw from 'tailwind.macro';
-import logo from '../../assets/logo.svg';
 import GlobalStyle from '../../theme/global-style';
 
 const StyledApp = styled.div`
   ${tw`text-center`}
 `;
 
-const rotate360 = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const AppLogo = styled.img`
-  ${tw`pointer-events-none`}
-  animation: ${rotate360} infinite 120s linear;
-  height: 40vmin;
-`;
-
 const AppHeader = styled.div`
-  ${tw`
-    bg-gray-800
-    min-h-screen
-    flex
-    flex-col
-    items-center
-    justify-center
-    text-white
-  `}
+  ${tw`bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white`}
   font-size: calc(10px + 2vmin);
-`;
-
-const AppLink = styled.a`
-  ${tw`text-blue-500`}
 `;
 
 const App: React.FC = () => {
@@ -45,13 +17,9 @@ const App: React.FC = () => {
     <StyledApp>
       <GlobalStyle />
       <AppHeader>
-        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </AppLink>
       </AppHeader>
     </StyledApp>
   );

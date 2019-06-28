7348cc5024fa55ddff7acf0468099dbccbfe044b
Ben Walker <bwalke05@gmail.com>
2019-06-28 18:00:44 -0400

    Connect react to apollo-client/server

--- capsules/moon-dust/src/index.tsx
+++ capsules/moon-dust/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import apolloClient from './config/apollo-client';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WrappedApp: React.FC = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  );
+};
 
-/* unregister() -> register() for offline capabilities and faster load
-times; comes with some pitfalls.
-Learn more: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app */
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
 serviceWorker.unregister();

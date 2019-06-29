d8de618ad44899a178da4110c101a46e967903cf
Ben Walker <bwalke05@gmail.com>
2019-06-28 20:07:44 -0400

    Add backup fonts in case those specified are unsupported

--- capsules/moon-dust/src/theme/index-style.ts
+++ capsules/moon-dust/src/theme/index-style.ts
@@ -3,13 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    font-family: 'Open Sans', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Open Sans', 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
   code {
-    font-family: 'Source Code Pro', monospace;
+    font-family: 'Source Code Pro', Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
 `;
 

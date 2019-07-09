e3e05d093001708df5f67a765b8fe4af4b4062ee
Ben Walker <bwalke05@gmail.com>
2019-07-09 19:41:07 -0400

    Prioritize open sans as font instead of system ui

--- capsules/moon-dust/src/theme/base-style.ts
+++ capsules/moon-dust/src/theme/base-style.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Open Sans', 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }

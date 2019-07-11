02934658f557d92c5332a8c9c4b84024393ba962
Ben Walker <bwalke05@gmail.com>
2019-07-11 19:03:25 -0400

    Remove global styling from App

--- capsules/moon-dust/src/components/App/App.tsx
+++ capsules/moon-dust/src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
-import BaseStyle from '../../theme/base-style';
 
 const StyledApp = styled.div`
   ${tw`text-center`}
@@ -16,7 +15,6 @@ const AppHeader = styled.div`
 const App: React.FC = () => {
   return (
     <StyledApp>
-      <BaseStyle />
       <AppHeader>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.

6854a24a74623c36546d10bc483eecb574fa3a42
Ben Walker <bwalke05@gmail.com>
2019-07-13 17:03:57 -0400

    Scaffold Label component

---
+++ capsules/moon-dust/src/components/Label/Label.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import tw from 'tailwind.macro';
+
+const StyledLabel = styled.div`
+  ${tw`
+    block
+    text-left
+  `}
+`;
+
+const StyledP = styled.p`
+  ${tw`
+    select-none
+    text-base
+    font-semibold
+    mb-2
+  `}
+`;
+
+export interface IProps {
+  text: string;
+}
+
+const Label: React.FC<IProps> = props => {
+  return (
+    <StyledLabel>
+      <StyledP>{props.text}</StyledP>
+      {props.children}
+    </StyledLabel>
+  );
+};
+
+export default Label;

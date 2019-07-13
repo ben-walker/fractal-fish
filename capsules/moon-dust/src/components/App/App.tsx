384ea9c339a3adfc488284b59c7ecf4415238d07
Ben Walker <bwalke05@gmail.com>
2019-07-13 19:57:56 -0400

    Remove extraneous components from App

--- capsules/moon-dust/src/components/App/App.tsx
+++ capsules/moon-dust/src/components/App/App.tsx
@@ -3,28 +3,11 @@ import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
 
 const StyledApp = styled.div`
-  ${tw`text-center`}
-`;
-
-const AppHeader = styled.div`
-  ${tw`min-h-screen flex flex-col items-center justify-center`}
-  color: black;
-  font-size: calc(10px + 2vmin);
-`;
-
-const StyledP = styled.p`
-  ${tw`font-semibold`}
+  ${tw``}
 `;
 
 const App: React.FC = () => {
-  return (
-    <StyledApp>
-      <AppHeader>
-        <StyledP>Edit and save to reload.</StyledP>
-        <code>Here's some code - int p = 5;</code>
-      </AppHeader>
-    </StyledApp>
-  );
+  return <StyledApp />;
 };
 
 export default App;

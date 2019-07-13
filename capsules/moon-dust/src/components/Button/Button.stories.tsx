b8a35b329a44998b5c1838291afd5e6c1ba3916d
Ben Walker <bwalke05@gmail.com>
2019-07-12 22:46:07 -0400

    Remove usage of Button props interface

--- capsules/moon-dust/src/components/Button/Button.stories.tsx
+++ capsules/moon-dust/src/components/Button/Button.stories.tsx
@@ -1,9 +1,5 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Button, { IProps } from './Button';
+import Button from './Button';
 
-const defButton: IProps = {
-  text: 'Default Button',
-};
-
-storiesOf('Button', module).add('default', () => <Button text={defButton.text} />);
+storiesOf('Button', module).add('default', () => <Button text="Default Button" />);

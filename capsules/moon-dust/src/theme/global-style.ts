8606e5e38962cc250f1c6d6a647a8a1da573cca4
Ben Walker <bwalke05@gmail.com>
2019-07-12 20:09:26 -0400

    Use helvetica neue and courier new instead of getting all fancy

--- capsules/moon-dust/src/theme/global-style.ts
+++ capsules/moon-dust/src/theme/global-style.ts
@@ -1,17 +1,15 @@
 import { createGlobalStyle } from 'styled-components/macro';
-import 'typeface-open-sans';
-import 'typeface-source-code-pro';
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    font-family: 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
   code {
-    font-family: 'Source Code Pro', Menlo, Monaco, Consolas, 'Courier New', monospace;
+    font-family: 'Courier New', monospace;
   }
 
   button {

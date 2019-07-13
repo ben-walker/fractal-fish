1df5a721b9a2c71c7da182658e9496959c316735
Ben Walker <bwalke05@gmail.com>
2019-07-12 22:45:24 -0400

    Refine text input component

--- capsules/moon-dust/src/components/TextInput/TextInput.stories.tsx
+++ capsules/moon-dust/src/components/TextInput/TextInput.stories.tsx
@@ -1,11 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import TextInput, { IProps } from './TextInput';
+import TextInput from './TextInput';
 
-const defTextInput: IProps = {
-  placeholder: 'Default Input',
-};
-
-storiesOf('TextInput', module).add('default', () => (
-  <TextInput placeholder={defTextInput.placeholder} />
-));
+storiesOf('TextInput', module)
+  .add('default', () => <TextInput placeholder="Placeholder" type="text" />)
+  .add('password', () => <TextInput placeholder="Password" type="password" />);
--- capsules/moon-dust/src/components/TextInput/TextInput.tsx
+++ capsules/moon-dust/src/components/TextInput/TextInput.tsx
@@ -3,15 +3,27 @@ import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
 
 const StyledTextInput = styled.input`
-  ${tw``}
+  ${tw`
+    rounded-lg
+    outline-none
+    border-solid
+    border-4
+    border-purple-500
+    px-2
+    py-2
+    font-bold
+    text-sm
+    shadow-lg
+  `}
 `;
 
 export interface IProps {
   placeholder: string;
+  type: string;
 }
 
 const TextInput: React.FC<IProps> = props => {
-  return <StyledTextInput placeholder={props.placeholder} />;
+  return <StyledTextInput placeholder={props.placeholder} type={props.type} />;
 };
 
 export default TextInput;

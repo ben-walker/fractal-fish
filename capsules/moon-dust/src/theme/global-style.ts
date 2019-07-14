40b416cfaffeb25f77aa6ba74f140d455395a009
Ben Walker <bwalke05@gmail.com>
2019-07-13 21:38:36 -0400

    Set margin to 0 on global style

--- capsules/moon-dust/src/theme/global-style.ts
+++ capsules/moon-dust/src/theme/global-style.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components/macro';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    margin: 0;
     font-family: 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;

95fe41f2c1fe813ce1b133b234e836c90769c005
Ben Walker <bwalke05@gmail.com>
2019-07-19 21:17:36 -0400

    Initialize socket connection alongside graphql endpoint

--- capsules/outer-space/src/server.ts
+++ capsules/outer-space/src/server.ts
@@ -1,31 +1,16 @@
-import { ApolloServer } from 'apollo-server-express';
 import * as http from 'http';
+import * as socketIo from 'socket.io';
 import app from './app';
-import corsOpts from './config/cors-opts';
-import { prisma } from './generated/prisma-client';
-import graphqlSchema from './graphql-schema';
-import { normalizePort, onError, onListening } from './server-utils';
+import apolloServer from './config/apollo-server';
+import winston from './config/winston';
 
-const PORT = normalizePort(process.env.PORT || '3000');
-app.set('port', PORT);
-
-const apollo = new ApolloServer({
-  context: ({ req }) => ({
-    ...req,
-    db: prisma,
-  }),
-  schema: graphqlSchema,
-});
-
-apollo.applyMiddleware({
-  app,
-  cors: corsOpts,
-  path: '/graph',
-});
+apolloServer.applyMiddleware({ app });
 
 const server = http.createServer(app);
-server.listen({ port: PORT });
-server.on('listening', () => onListening(server));
-server.on('error', e => onError(e, PORT));
 
-export default server;
+export const io = socketIo(server);
+
+const port = '4000';
+server.listen({ port }, () =>
+  winston.debug(`🚀  Server ready at http://localhost:${port}${apolloServer.graphqlPath}`)
+);

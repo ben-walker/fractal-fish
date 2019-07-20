953303e37e1490e021c1d869e045f703ed090540
Ben Walker <bwalke05@gmail.com>
2019-07-19 21:14:21 -0400

    Configure cors options on express server

--- capsules/outer-space/src/app.ts
+++ capsules/outer-space/src/app.ts
@@ -1,10 +1,13 @@
+import * as cors from 'cors';
 import * as express from 'express';
 import * as helmet from 'helmet';
 import * as morgan from 'morgan';
+import corsOpts from './config/cors-opts';
 import morganOpts from './config/morgan-opts';
 
 const app = express();
 app.use(helmet());
 app.use(morgan('combined', morganOpts));
+app.use(cors(corsOpts));
 
 export default app;

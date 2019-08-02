671995695ddc9c9ce669fe920aa2c169dcc214d8
Ben Walker <bwalke05@gmail.com>
2019-08-02 17:13:10 -0400

    Simplify NavigationBar component

--- capsules/moon-dust/src/components/NavigationBar/NavigationBar.tsx
+++ capsules/moon-dust/src/components/NavigationBar/NavigationBar.tsx
@@ -1,45 +1,23 @@
-import React, { useState } from 'react';
-import { animated, config, useSpring } from 'react-spring';
+import React from 'react';
 import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
 
-const StyledNavigationBar = styled(animated.ul)`
+const StyledNavigationBar = styled.ul`
   ${tw`
-    p-0
-    m-0
-    bg-transparent
-    border-solid
-    border-0
-    border-transparent
-    shadow-lg
     fixed
     bottom-0
-    right-0
-    rounded
+    left-0
     h-full
+    w-56
+    p-0
+    m-0
+    bg-gray-100
   `}
   list-style-type: none;
-  will-change: width;
 `;
 
 const NavigationBar: React.FC = props => {
-  const [isExpanded, setExpanded] = useState(false);
-  const [big, little] = ['14rem', '4rem'];
-
-  const animatedStyle = useSpring({
-    config: config.stiff,
-    width: isExpanded ? big : little,
-  });
-
-  return (
-    <StyledNavigationBar
-      onMouseEnter={() => setExpanded(true)}
-      onMouseLeave={() => setExpanded(false)}
-      style={animatedStyle}
-    >
-      {props.children}
-    </StyledNavigationBar>
-  );
+  return <StyledNavigationBar>{props.children}</StyledNavigationBar>;
 };
 
 export default NavigationBar;

b8da830a4876f7f7d62f064895c9ccdc07c9481e
Ben Walker <bwalke05@gmail.com>
2019-08-03 23:47:03 -0400

    Refactor TextInput with support for theming and knobs

--- capsules/moon-dust/src/components/TextInput/TextInput.stories.tsx
+++ capsules/moon-dust/src/components/TextInput/TextInput.stories.tsx
@@ -1,7 +1,21 @@
+import { optionsKnob as options, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import TextInput from './TextInput';
 
-storiesOf('TextInput', module)
-  .add('Default', () => <TextInput placeholder="Placeholder" />)
-  .add('Password', () => <TextInput placeholder="Password" type="password" />);
+storiesOf('TextInput', module).add('Default', () => (
+  <TextInput
+    variant={options('Variant', { default: 'default', primary: 'primary' }, 'default', {
+      display: 'select',
+    })}
+    placeholder={text('Placeholder', 'Placeholder')}
+    type={options(
+      'Type',
+      { email: 'email', text: 'text', password: 'password', number: 'number' },
+      'text',
+      {
+        display: 'select',
+      }
+    )}
+  />
+));
--- capsules/moon-dust/src/components/TextInput/TextInput.tsx
+++ capsules/moon-dust/src/components/TextInput/TextInput.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import theme from 'styled-theming';
 import tw from 'tailwind.macro';
+
+const color = theme.variants('mode', 'variant', {
+  default: { light: '#96ACB7', dark: '' },
+  primary: { light: '#0496FF', dark: '' },
+});
 
 const StyledTextInput = styled.input`
   ${tw`
+    outline-none
     rounded
     border-solid
     border
-    border-gray-500
     px-3
     py-3
-    font-regular
-    text-sm
+    font-medium
+    text-base
   `}
+  border-width: 3px;
+  border-color: ${color};
 `;
 
 export interface ITextInput extends React.HTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   type?: 'text' | 'password' | 'email' | 'number';
+  variant?: 'default' | 'primary';
 }
 
 const TextInput: React.FC<ITextInput> = props => {
-  return <StyledTextInput placeholder={props.placeholder} type={props.type} />;
+  return <StyledTextInput {...props} />;
 };
 
 TextInput.defaultProps = {
   type: 'text',
+  variant: 'default',
 };
 
 export default TextInput;

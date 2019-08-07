1bbac71a7b26721f48d6daf46f2bbf0b757fd43b
Ben Walker <bwalke05@gmail.com>
2019-08-06 20:32:30 -0400

    Refactor text button; add knobs options

--- capsules/moon-dust/src/components/TextButton/TextButton.stories.tsx
+++ capsules/moon-dust/src/components/TextButton/TextButton.stories.tsx
@@ -1,8 +1,15 @@
+import { optionsKnob as options, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { Hash } from 'react-feather';
+import { DownloadCloud } from 'react-feather';
 import TextButton from './TextButton';
 
-storiesOf('TextButton', module)
-  .add('Default', () => <TextButton text="Default TextButton" />)
-  .add('With Icon', () => <TextButton text="With Icon" icon={<Hash size={20} />} />);
+storiesOf('TextButton', module).add('Default', () => (
+  <TextButton
+    variant={options('Variant', { default: 'default', primary: 'primary' }, 'default', {
+      display: 'select',
+    })}
+    text={text('Text', 'Default Button')}
+    icon={<DownloadCloud size={18} />}
+  />
+));
--- capsules/moon-dust/src/components/TextButton/TextButton.tsx
+++ capsules/moon-dust/src/components/TextButton/TextButton.tsx
@@ -1,6 +1,18 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components/macro';
+import theme from 'styled-theming';
 import tw from 'tailwind.macro';
+
+const color = theme.variants('mode', 'variant', {
+  default: { light: '#96ACB7', dark: '' },
+  primary: { light: '#0496FF', dark: '' },
+});
+
+export interface ITextButton extends React.HTMLAttributes<HTMLButtonElement> {
+  text?: string;
+  icon?: ReactNode;
+  variant?: 'default' | 'primary';
+}
 
 const StyledTextButton = styled.button`
   ${tw`
@@ -9,31 +21,30 @@ const StyledTextButton = styled.button`
     border-none
     bg-transparent
     font-semibold
-    text-sm
+    text-base
     cursor-pointer
-    text-orange-500
-    hover:text-orange-400
+    hover:opacity-75
     flex
     items-center
   `}
+  color: ${color};
 `;
 
 const StyledIcon = styled.div`
   padding-right: 0.3rem;
 `;
 
-export interface ITextButton extends React.HTMLAttributes<HTMLButtonElement> {
-  text?: string;
-  icon?: ReactNode;
-}
-
 const TextButton: React.FC<ITextButton> = props => {
   return (
-    <StyledTextButton>
+    <StyledTextButton {...props}>
       {props.icon && <StyledIcon>{props.icon}</StyledIcon>}
       {props.text}
     </StyledTextButton>
   );
+};
+
+TextButton.defaultProps = {
+  variant: 'default',
 };
 
 export default TextButton;

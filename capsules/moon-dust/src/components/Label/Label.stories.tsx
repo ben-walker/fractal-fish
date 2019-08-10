ff466557e8eef3001e29858b886429602655ce73
Ben Walker <bwalke05@gmail.com>
2019-08-10 16:12:04 -0400

    Remove TextInput from Label

--- capsules/moon-dust/src/components/Label/Label.stories.tsx
+++ capsules/moon-dust/src/components/Label/Label.stories.tsx
@@ -1,12 +1,5 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import TextInput from '../TextInput/TextInput';
 import Label from './Label';
 
-storiesOf('Label', module)
-  .add('Default', () => <Label text="Default Label" />)
-  .add('With TextInput', () => (
-    <Label text="With TextInput">
-      <TextInput placeholder="Testing..." />
-    </Label>
-  ));
+storiesOf('Label', module).add('Default', () => <Label text="Default Label" />);

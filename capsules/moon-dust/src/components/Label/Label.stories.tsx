fd4ea9b359518a09a33c26ee75d9e9d3388657f9
Ben Walker <bwalke05@gmail.com>
2019-08-11 12:32:16 -0400

    Add knob for label text

--- capsules/moon-dust/src/components/Label/Label.stories.tsx
+++ capsules/moon-dust/src/components/Label/Label.stories.tsx
@@ -1,5 +1,6 @@
+import { text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Label from './Label';
 
-storiesOf('Label', module).add('Default', () => <Label text="Default Label" />);
+storiesOf('Label', module).add('Default', () => <Label text={text('Text', 'Default Label')} />);

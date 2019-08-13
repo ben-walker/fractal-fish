956bf57ce3dabaff4412d3fd373b6166319f7a7c
Ben Walker <bwalke05@gmail.com>
2019-08-12 21:23:54 -0400

    Add mobile menu indicator icon with open state

--- capsules/moon-dust/src/components/NavigationBar/MobileNavigationBar/MobileNavigationBar.tsx
+++ capsules/moon-dust/src/components/NavigationBar/MobileNavigationBar/MobileNavigationBar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu } from 'react-feather';
 import styled from 'styled-components/macro';
+import tw from 'tailwind.macro';
 
-const StyledMobileNavigationBar = styled.ul``;
+const ToggleContainer = styled.div`
+  ${tw`
+    fixed
+    top-0
+    left-0
+    m-3
+    cursor-pointer
+  `}
+`;
 
 const MobileNavigationBar: React.FC = props => {
-  return <StyledMobileNavigationBar>{props.children}</StyledMobileNavigationBar>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ToggleContainer onClick={() => setOpen(!open)}>
+      <Menu color="#4a5568" />
+    </ToggleContainer>
+  );
 };
 
 export default MobileNavigationBar;

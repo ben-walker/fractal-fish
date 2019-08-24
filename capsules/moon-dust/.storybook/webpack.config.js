bac0dc9297223e35c36cd48e4d506864f250c2a9
Ben Walker <bwalke05@gmail.com>
2019-08-24 19:23:45 -0400

    Support svg loading from storybook webpack

--- capsules/moon-dust/.storybook/webpack.config.js
+++ capsules/moon-dust/.storybook/webpack.config.js
@@ -1,14 +1,21 @@
 module.exports = async ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: 'babel-loader',
-    options: {
-      presets: [['react-app', { flow: false, typescript: true }]],
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
     },
-  });
+    {
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    }
+  );
+
   config.resolve.extensions.push('.ts', '.tsx');
-  config.node = {
-    fs: 'empty',
-  };
+
+  config.node = { fs: 'empty' };
+
   return config;
 };

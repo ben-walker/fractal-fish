86d3a7339fc5845922d7fc89944dc433c3a08662
Ben Walker <bwalke05@gmail.com>
2019-08-28 19:16:34 -0400

    Update svg type definition

--- capsules/moon-dust/src/types/svg.d.ts
+++ capsules/moon-dust/src/types/svg.d.ts
@@ -1,6 +1,5 @@
-import React from 'react';
-
 declare module '*.svg' {
+  import React from 'react';
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;

1b5cd97addc7172fb640b1696b4bd4313195b453
Ben Walker <bwalke05@gmail.com>
2019-08-29 19:13:17 -0400

    Reinstate svg type definition

--- capsules/moon-dust/src/types/svg.d.ts
+++ capsules/moon-dust/src/types/svg.d.ts
@@ -1,7 +1,6 @@
-/* declare module '*.svg' {
+declare module '*.svg' {
   import React from 'react';
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
 }
- */

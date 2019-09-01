83929ae86e6f464d33f72372ff5977f3e1080537
Ben Walker <bwalke05@gmail.com>
2019-09-01 12:42:16 -0400

    Define text input variants

--- capsules/moon-dust/src/theme/variant-map.ts
+++ capsules/moon-dust/src/theme/variant-map.ts
@@ -11,3 +11,11 @@ export const buttonHoverVariant = theme.variants(mode, variant, {
   default: { light: '#525252', dark: '' },
   primary: { light: '#0586e3', dark: '' },
 });
+
+export const textInputVariant = theme.variants(mode, variant, {
+  default: { light: '#a8a8a8', dark: '' },
+});
+
+export const textInputFocusVariant = theme.variants(mode, variant, {
+  default: { light: '#525252', dark: '' },
+});

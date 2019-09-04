b555ba35c0d3fde7412b4b000d35ccaeebae6286
Ben Walker <bwalke05@gmail.com>
2019-09-04 18:52:28 -0400

    Remove default padding around every story

--- capsules/moon-dust/.storybook/StyleDecorator.tsx
+++ capsules/moon-dust/.storybook/StyleDecorator.tsx
@@ -5,17 +5,11 @@ import styled from 'styled-components/macro';
 import WindowDimensionsProvider from '../src/hooks/WindowDimensions/WindowDimensions';
 import GlobalStyle from '../src/theme/global-style';
 
-const StyledWrapper = styled.div`
-  padding: 0.75rem;
-`;
-
 const StyleDecorator = (storyFn: RenderFunction) => (
   <>
     <GlobalStyle />
     <ThemeProvider theme={{ mode: 'light' }}>
-      <WindowDimensionsProvider>
-        <StyledWrapper>{storyFn()}</StyledWrapper>
-      </WindowDimensionsProvider>
+      <WindowDimensionsProvider>{storyFn()}</WindowDimensionsProvider>
     </ThemeProvider>
   </>
 );

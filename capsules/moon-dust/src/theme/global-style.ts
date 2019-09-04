b91b2f31db16f99b444da4e14c69d02670d0b4e1
Ben Walker <bwalke05@gmail.com>
2019-09-03 20:56:18 -0400

    Revert to react default fonts

--- capsules/moon-dust/src/theme/global-style.ts
+++ capsules/moon-dust/src/theme/global-style.ts
@@ -4,7 +4,8 @@ import 'typeface-ibm-plex-sans';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    font-family: 'IBM Plex Sans', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }

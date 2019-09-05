773c77e3180d1ac04aa8809187efabb2367f877e
Ben Walker <bwalke05@gmail.com>
2019-09-04 20:04:53 -0400

    Redesign mobile navigation

--- capsules/moon-dust/src/components/NavigationBar/MobileNavigationBar/MobileNavigationBar.tsx
+++ capsules/moon-dust/src/components/NavigationBar/MobileNavigationBar/MobileNavigationBar.tsx
@@ -1,48 +1,51 @@
-import React from 'react';
-import { Menu } from 'react-feather';
+import React, { useState } from 'react';
+import { ChevronDown } from 'react-feather';
 import { animated, config, useSpring } from 'react-spring';
 import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
-import { useComponentVisible } from '../../../hooks/ComponentVisible/ComponentVisible';
 
-const StyledMenu = styled(Menu)`
+const OverlayToggle = styled(animated(ChevronDown))`
   ${tw`
     fixed
     top-0
     left-0
-    m-3
+    m-2
     cursor-pointer
+    z-10
   `}
-  color: #4a5568;
+  color: #525252;
 `;
 
-const SideBar = styled(animated.div)`
+const Overlay = styled(animated.div)`
   ${tw`
     fixed
-    bottom-0
-    left-0
-    h-full
+    top-0
+    w-full
     p-0
     m-0
-    shadow
     list-none
   `}
   background-color: #f7f9fa;
 `;
 
 const MobileNavigationBar: React.FC = props => {
-  const { ref, open, setOpen } = useComponentVisible(false);
-  const [closedWidth, openWidth] = ['0rem', '14rem'];
+  const [open, setOpen] = useState(false);
+  const [closedHeight, openHeight] = ['0%', '100%'];
 
-  const spring = useSpring({
+  const overlaySpring = useSpring({
     config: config.default,
-    width: open ? openWidth : closedWidth,
+    height: open ? openHeight : closedHeight,
+  });
+
+  const toggleSpring = useSpring({
+    config: config.stiff,
+    transform: `rotate(${open ? 180 : 0}deg)`,
   });
 
   return (
     <>
-      <StyledMenu onClick={() => setOpen(true)} />
-      <SideBar ref={ref} style={spring} />
+      <OverlayToggle style={toggleSpring} onClick={() => setOpen(state => !state)} />
+      <Overlay style={overlaySpring} />
     </>
   );
 };

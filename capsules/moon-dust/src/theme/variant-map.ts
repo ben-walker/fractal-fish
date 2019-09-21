58fed44cb076e960340d0df90aa0e39ca34e87ea
Ben Walker <bwalke05@gmail.com>
2019-09-21 13:31:27 -0400

    Remove old text input colour variants

--- capsules/moon-dust/src/theme/variant-map.ts
+++ capsules/moon-dust/src/theme/variant-map.ts
@@ -6,11 +6,3 @@ export const buttonVariant = theme.variants(mode, variant, {
   default: { light: '#a8a8a8', dark: '' },
   primary: { light: '#0496ff', dark: '' },
 });
-
-export const textInputVariant = theme.variants(mode, variant, {
-  default: { light: '#a8a8a8', dark: '' },
-});
-
-export const textInputFocusVariant = theme.variants(mode, variant, {
-  default: { light: '#525252', dark: '' },
-});

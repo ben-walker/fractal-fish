0817ed542c8f3e52ea2cdf4e8306fd7c9a96b543
Ben Walker <bwalke05@gmail.com>
2019-11-16 16:49:37 -0500

    Restart server automatically with nodemon-webpack-plugin

--- capsules/potential-well/webpack.dev.ts
+++ capsules/potential-well/webpack.dev.ts
@@ -1,3 +1,5 @@
+import * as nodemonWebpackPlugin from 'nodemon-webpack-plugin';
+import * as path from 'path';
 import * as webpack from 'webpack';
 import * as webpackMerge from 'webpack-merge';
 import webpackBase from './webpack.base';
@@ -5,6 +7,15 @@ import webpackBase from './webpack.base';
 const config: webpack.Configuration = webpackMerge(webpackBase, {
   devtool: 'inline-source-map',
   mode: 'development',
+  plugins: [
+    new nodemonWebpackPlugin({
+      env: {
+        quiet: true,
+        script: path.resolve(__dirname, 'build/app.bundle.js'),
+        watch: [path.resolve(__dirname, 'build/app.bundle.js')],
+      },
+    }),
+  ],
   watch: true,
 });
 

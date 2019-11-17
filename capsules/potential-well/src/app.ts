dc693bdbada1f66c9c918308898481cecb5a887b
Ben Walker <bwalke05@gmail.com>
2019-11-16 22:03:05 -0500

    Use error catching in the middleware chain

--- capsules/potential-well/src/app.ts
+++ capsules/potential-well/src/app.ts
@@ -2,9 +2,10 @@ import * as compose from 'koa-compose';
 import * as Koa from 'koa';
 import * as helmet from 'koa-helmet';
 import * as logger from 'koa-logger';
+import errorCatcher from './middleware/error-catcher';
 
 const app = new Koa();
-const middleware = compose([helmet(), logger()]);
+const middleware = compose([helmet(), logger(), errorCatcher]);
 
 app.use(middleware);
 

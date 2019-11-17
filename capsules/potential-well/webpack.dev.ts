f4eea76cb2961f3236de03396bb1860da8c1bba4
Ben Walker <bwalke05@gmail.com>
2019-11-16 21:34:07 -0500

    Refine imports

--- capsules/potential-well/webpack.dev.ts
+++ capsules/potential-well/webpack.dev.ts
@@ -1,14 +1,14 @@
-import * as nodemonWebpackPlugin from 'nodemon-webpack-plugin';
 import * as path from 'path';
 import * as webpack from 'webpack';
 import * as webpackMerge from 'webpack-merge';
+import * as NodemonWebpackPlugin from 'nodemon-webpack-plugin';
 import webpackBase from './webpack.base';
 
 const config: webpack.Configuration = webpackMerge(webpackBase, {
   devtool: 'inline-source-map',
   mode: 'development',
   plugins: [
-    new nodemonWebpackPlugin({
+    new NodemonWebpackPlugin({
       env: {
         quiet: true,
         script: path.resolve(__dirname, 'build/app.bundle.js'),

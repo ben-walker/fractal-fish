f50d49ee217c6b5ba7b03918d316bbf94a919e88
Ben Walker <bwalke05@gmail.com>
2019-11-17 22:08:35 -0500

    Use a class-based approach to db connection

---
+++ capsules/potential-well/src/config/db/instance.ts
@@ -0,0 +1,36 @@
+import * as mongoose from 'mongoose';
+
+const options: mongoose.ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+class DBInstance {
+  private uri: string;
+  private connection: mongoose.Connection;
+
+  constructor(uri: string) {
+    this.uri = uri;
+    this.connection = mongoose.connection;
+  }
+
+  public connect(): void {
+    mongoose.connect(this.uri, options).catch(err => {
+      console.error(err.stack);
+      process.exit(1);
+    });
+  }
+
+  public disconnect(): void {
+    this.connection.close().catch(err => {
+      console.error(err.stack);
+      process.exit(1);
+    });
+  }
+
+  public getConnection(): mongoose.Connection {
+    return this.connection;
+  }
+}
+
+export default DBInstance;

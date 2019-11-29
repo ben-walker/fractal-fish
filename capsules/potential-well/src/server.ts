a7eed1aaef95db10b601c19c2b044dda35bcd2fd
Ben Walker <bwalke05@gmail.com>
2019-11-29 18:50:37 -0500

    Where once there was ws now there is socket.io

--- capsules/potential-well/src/server.ts
+++ capsules/potential-well/src/server.ts
@@ -1,7 +1,13 @@
 import http from 'http';
+import SocketIO from 'socket.io';
 import app from './app';
 
 const server = http.createServer(app.callback());
+const io = SocketIO(server);
 const port = '3000' || process.env.PORT;
+
+io.on('connection', socket => {
+  socket.send('hello world');
+});
 
 server.listen({ port }, () => console.log(`🚀  Server ready at http://localhost:${port}`));

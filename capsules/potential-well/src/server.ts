80756290108f2a66ebc017072c879d963ff6b56c
Ben Walker <bwalke05@gmail.com>
2019-11-29 18:08:23 -0500

    Remove ws usage

--- capsules/potential-well/src/server.ts
+++ capsules/potential-well/src/server.ts
@@ -1,13 +1,7 @@
 import http from 'http';
-import Websocket from 'ws';
 import app from './app';
 
 const server = http.createServer(app.callback());
-const wss = new Websocket.Server({ server });
 const port = '3000' || process.env.PORT;
-
-wss.on('connection', ws => {
-  ws.send('hello world');
-});
 
 server.listen({ port }, () => console.log(`🚀  Server ready at http://localhost:${port}`));

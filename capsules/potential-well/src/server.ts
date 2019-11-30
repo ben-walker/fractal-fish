a91a5ce34e0248dfe425e5e43c5a1ed5bef04386
Ben Walker <bwalke05@gmail.com>
2019-11-29 19:08:09 -0500

    Use socket.io redis

--- capsules/potential-well/src/server.ts
+++ capsules/potential-well/src/server.ts
@@ -1,9 +1,11 @@
 import http from 'http';
 import SocketIO from 'socket.io';
+import redisAdapter from 'socket.io-redis';
 import app from './app';
 
 const server = http.createServer(app.callback());
 const io = SocketIO(server);
+io.adapter(redisAdapter());
 const port = '3000' || process.env.PORT;
 
 io.on('connection', socket => {

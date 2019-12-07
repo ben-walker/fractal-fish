0bf60435fce2f839421f48cc05c955619ae8728e
Ben Walker <bwalke05@gmail.com>
2019-12-07 14:07:25 -0500

    Remove nodemon-webpack-plugin

--- capsules/potential-well/webpack/webpack.dev.ts
+++ capsules/potential-well/webpack/webpack.dev.ts
@@ -1,6 +1,5 @@
 import { Configuration } from 'webpack';
 import Dotenv from 'dotenv-webpack';
-import Nodemon from 'nodemon-webpack-plugin';
 import base from './webpack.base';
 import merge from 'webpack-merge';
 import path from 'path';
@@ -8,17 +7,7 @@ import path from 'path';
 const config: Configuration = merge(base, {
   devtool: 'inline-source-map',
   mode: 'development',
-  plugins: [
-    new Dotenv({ path: path.resolve('.env.local') }),
-    new Nodemon({
-      env: {
-        quiet: true,
-        script: path.resolve('build/app.bundle.js'),
-        watch: [path.resolve('build/app.bundle.js')],
-      },
-    }),
-  ],
-  watch: true,
+  plugins: [new Dotenv({ path: path.resolve('.env.local') })],
 });
 
 export default config;

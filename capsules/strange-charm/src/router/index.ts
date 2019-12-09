a2e713b4b82d00f7c532a2b6d6a54f6f4f43dff1
Ben Walker <bwalke05@gmail.com>
2019-12-08 19:15:23 -0500

    Clean up vue app

--- capsules/strange-charm/src/router/index.ts
+++ capsules/strange-charm/src/router/index.ts
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
+const routes: RouteConfig[] = [
   {
     path: "/",
     name: "home",
@@ -13,11 +13,7 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("../views/About.vue")
   }
 ];
 

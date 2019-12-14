f4816e3bdce96344e66076f09219cc7b1121b15d
Ben Walker <bwalke05@gmail.com>
2019-12-13 23:23:10 -0500

    Better debug logging

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -60,7 +60,7 @@ do
   PACKAGE_PATH=.${ROOT#.}/$PACKAGE
   LATEST_COMMIT_SINCE_LAST_BUILD=$(git log -1 $CIRCLE_SHA1 ^$LAST_COMPLETED_BUILD_SHA --format=format:%H --full-diff ${PACKAGE_PATH#/})
 
-  echo "${PACKAGE_PATH}"
+  echo "${PACKAGE_PATH#/}"
   echo "${CIRCLE_SHA1}"
   echo "${LAST_COMPLETED_BUILD_SHA}"
   echo "${LATEST_COMMIT_SINCE_LAST_BUILD}"

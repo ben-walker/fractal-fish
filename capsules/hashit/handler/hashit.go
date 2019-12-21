46eb107bacac3c5aec4fa0a6c98764bf376bf089
Ben Walker <bwalke05@gmail.com>
2019-12-21 13:45:33 -0500

    Send encoded hash string in response

--- capsules/hashit/handler/hashit.go
+++ capsules/hashit/handler/hashit.go
@@ -3,8 +3,7 @@ package handler
 import (
 	"context"
 
-	"github.com/micro/go-micro/util/log"
-
+	"hashit/lib"
 	hashit "hashit/proto/hashit"
 )
 
@@ -13,7 +12,17 @@ type Hashit struct{}
 
 // Crypt is a single request handler called via client.Call or the generated client code
 func (e *Hashit) Crypt(ctx context.Context, req *hashit.Request, rsp *hashit.Response) error {
-	log.Log("Received Hashit.Crypt request")
-	rsp.Hashed = req.Value
+	p := &hasher.Params{
+		Memory:      64 * 1024,
+		Iterations:  3,
+		Parallelism: 2,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+	hash, err := hasher.GenerateHash(req.Value, p)
+	if err != nil {
+		return err
+	}
+	rsp.Hash = hash
 	return nil
 }

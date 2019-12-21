b9adb670df9d03c1adb0ebe97d4674e00ff13b7d
Ben Walker <bwalke05@gmail.com>
2019-12-20 23:37:09 -0500

    Simplify main service

--- capsules/auth/main.go
+++ capsules/auth/main.go
@@ -1,34 +1,24 @@
 package main
 
 import (
-	"github.com/micro/go-micro/util/log"
-	"github.com/micro/go-micro"
 	"auth/handler"
-	"auth/subscriber"
-
 	auth "auth/proto/auth"
+	"auth/subscriber"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/util/log"
 )
 
 func main() {
-	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.srv.auth"),
 		micro.Version("latest"),
 	)
-
-	// Initialise service
 	service.Init()
 
-	// Register Handler
 	auth.RegisterAuthHandler(service.Server(), new(handler.Auth))
-
-	// Register Struct as Subscriber
 	micro.RegisterSubscriber("go.micro.srv.auth", service.Server(), new(subscriber.Auth))
-
-	// Register Function as Subscriber
 	micro.RegisterSubscriber("go.micro.srv.auth", service.Server(), subscriber.Handler)
 
-	// Run service
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}

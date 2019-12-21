a4867ac341ff97cb97af142cff2787873134bf73
Ben Walker <bwalke05@gmail.com>
2019-12-21 12:43:56 -0500

    Update formatting, reduce service interface

---
+++ capsules/hashit/go.mod
@@ -0,0 +1,9 @@
+module hashit
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-plugins v1.5.1
+)
--- capsules/hashit/main.go
+++ capsules/hashit/main.go
@@ -1,34 +1,23 @@
 package main
 
 import (
+	"hashit/handler"
+
 	"github.com/micro/go-micro/util/log"
 	"github.com/micro/go-micro"
-	"hashit/handler"
-	"hashit/subscriber"
 
 	hashit "hashit/proto/hashit"
 )
 
 func main() {
-	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.srv.hashit"),
-		micro.Version("latest"),
+		micro.Version("0.0.0"),
 	)
-
-	// Initialise service
 	service.Init()
 
-	// Register Handler
 	hashit.RegisterHashitHandler(service.Server(), new(handler.Hashit))
 
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("go.micro.srv.hashit", service.Server(), new(subscriber.Hashit))
-
-	// Register Function as Subscriber
-	micro.RegisterSubscriber("go.micro.srv.hashit", service.Server(), subscriber.Handler)
-
-	// Run service
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}

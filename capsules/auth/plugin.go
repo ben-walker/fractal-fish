5590dc747591323e63d86bd1697912ecc22a980e
Ben Walker <bwalke05@gmail.com>
2019-12-22 00:03:56 -0500

    Remove auth package

--- capsules/auth/plugin.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-	_ "github.com/micro/go-plugins/broker/grpc"
-	_ "github.com/micro/go-plugins/registry/kubernetes"
-	_ "github.com/micro/go-plugins/store/redis"
-	_ "github.com/micro/go-plugins/transport/grpc"
-)

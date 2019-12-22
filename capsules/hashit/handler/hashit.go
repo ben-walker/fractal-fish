3497f40f7ec0815d577e51761985136e81944979
Ben Walker <bwalke05@gmail.com>
2019-12-22 15:38:56 -0500

    Improve naming

--- capsules/hashit/handler/hashit.go
+++ capsules/hashit/handler/hashit.go
@@ -12,14 +12,14 @@ type Hashit struct{}
 
 // Encode is a single request handler called via client.Call or the generated client code
 func (e *Hashit) Encode(ctx context.Context, req *hashit.EncodeRequest, rsp *hashit.EncodeResponse) error {
-	p := &hasher.Params{
+	params := &hasher.Params{
 		Memory:      64 * 1024,
 		Iterations:  3,
 		Parallelism: 2,
 		SaltLength:  16,
 		KeyLength:   32,
 	}
-	hash, err := hasher.GenerateHash(req.Value, p)
+	hash, err := hasher.GenerateHash(req.Value, params)
 	if err != nil {
 		return err
 	}
